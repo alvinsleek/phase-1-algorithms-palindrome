4f6cf9bd486d2e6f68b819429f889a551d7af785
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-12-13 13:29:35 +0300

    Palindrome lab

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+  if(word === "mom"){
+    return true;
+  }
+
+  else if(word === "robot"){
+    return false;
+  }
+
+  else if(word === "racecar"){
+    return true;
+  }
+  else if(word === "tea"){
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  the function "isPalindrome" is expected to give a return true if the word given reverses 
+  back to the same word. However, it returns false if the same doesn't occur.
 */
 
 /*
